60cf6f23796647aacc2f709bd2f4f4d2126dd78b
seoungwonseoungwon <destiny_023@naver.com>
2023-07-28 16:01:23 +0900

    author

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import os
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
@@ -12,9 +13,11 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    author = models.ForeignKey(User,null=True, on_delete=models.SET_NULL)
+
 
     def __str__(self):
-        return f'[{self.pk}] {self.title}'
+        return f'[{self.pk}] {self.title} :: {self.author}'
     
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
