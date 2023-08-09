47325505963e9620bede893b2e85ae6b81c23a55
seoungwonseoungwon <destiny_023@naver.com>
2023-08-09 18:54:14 +0900

    마무라ㅣ

--- kbo/admin.py
+++ kbo/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import TeamList, Hitting, Pitcher
+from .models import TeamList, Hitting, Pitcher, PreRank
 # Register your models here.
 
 admin.site.register(TeamList)
 admin.site.register(Hitting)
 admin.site.register(Pitcher)
+admin.site.register(PreRank)
