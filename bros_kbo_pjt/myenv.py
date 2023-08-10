38c6f3957249c76640616c8ac2216f3fd9dbcdbc
seoungwonseoungwon <destiny_023@naver.com>
2023-08-10 12:04:00 +0900

    test

--- bros_kbo_pjt/myenv.py
+++ bros_kbo_pjt/myenv.py
@@ -31,6 +31,7 @@ DATABASES = {
 SECRET_KEY = 'django-insecure-d7h#e1-s@#=a*p1d6zv^8h%-^-lj4_1mjiedbq9fo11xbf*8f$'
 
 ### local mysql 연결임 
+### local mysql 연결임 
 
 # DATABASES = { 
 #     'default': {
